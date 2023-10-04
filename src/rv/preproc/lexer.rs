6348fe85c85cef48401a517bc8a8d1d00c91cc78
ryann <flipperplz@protonmail.com>
2023-10-03 18:02:10 -0700

    feat(pbo): Add RV Preprocessor Module

---
+++ src/rv/preproc/lexer.rs
@@ -0,0 +1,25 @@
+
+pub enum RvToken {
+    Include,
+    Define,
+    IfDef,
+    IfNDef,
+    Else,
+    EndIf,
+    Undef,
+    LeftParenthesis,
+    RightParenthesis,
+    Comma,
+    Hash,
+    NewLine,
+    LineComment,
+    BlockComment,
+    LineBreak,
+    DQuote,
+    LeftAngle,
+    RightAngle,
+    DHash,
+
+
+
+}
